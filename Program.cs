d9ad8de8af72c28c1654e90059b7c6cefd673607
Josias-Ribeiro <josias_ribeiro90@hotmail.com>
2022-07-20 15:48:53 -0300

    DDI - Criando estrutura e diretórios

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+var path = Path.Combine(Environment.CurrentDirectory, "globo");
+var dirGlobo = Directory.CreateDirectory(path);
+
+var dirAmNorte = dirGlobo.CreateSubdirectory("America do Norte");
+var dirAmCentral = dirGlobo.CreateSubdirectory("America Central");
+var dirAmSul = dirGlobo.CreateSubdirectory("America do Sul");
+
+dirAmNorte.CreateSubdirectory("USA");
+dirAmNorte.CreateSubdirectory("Mexico");
+dirAmNorte.CreateSubdirectory("Canada");
+
+dirAmCentral.CreateSubdirectory("Panama");
+
+dirAmSul.CreateSubdirectory("Brasil");
+dirAmSul.CreateSubdirectory("Paraguai");
