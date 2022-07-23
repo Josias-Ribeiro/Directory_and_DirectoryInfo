07ae6c37a17881d3463e2e5b87d521435ce84f56
Josias-Ribeiro <josias_ribeiro90@hotmail.com>
2022-07-23 03:53:08 -0300

    DDI - Ajustes em dll

--- Program.cs
+++ Program.cs
@@ -1,18 +1,61 @@
 ﻿using System;
 using System.IO;
 
-var path = Path.Combine(Environment.CurrentDirectory, "globo");
-var dirGlobo = Directory.CreateDirectory(path);
+CriarDiretoriosGlobo();
+CriarArquivo();
 
-var dirAmNorte = dirGlobo.CreateSubdirectory("America do Norte");
-var dirAmCentral = dirGlobo.CreateSubdirectory("America Central");
-var dirAmSul = dirGlobo.CreateSubdirectory("America do Sul");
+var origem = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+var destino = Path.Combine(Environment.CurrentDirectory, "globo", "America do Sul","Brasil", "brasil.txt");
 
-dirAmNorte.CreateSubdirectory("USA");
-dirAmNorte.CreateSubdirectory("Mexico");
-dirAmNorte.CreateSubdirectory("Canada");
+MoverArquivo(origem,destino);
 
-dirAmCentral.CreateSubdirectory("Panama");
+static void CopiarArquivo(string pathOrigem, string pathDestino){
+    File.Copy(pathOrigem,pathDestino);
+}
 
-dirAmSul.CreateSubdirectory("Brasil");
-dirAmSul.CreateSubdirectory("Paraguai");
+static void MoverArquivo(string pathOrigem, string pathDestino)
+{
+    if (!File.Exists(pathOrigem)){
+        Console.WriteLine("Arquivo de origem não existe");
+        return;
+    }
+
+    if (File.Exists(pathDestino)){
+        Console.WriteLine("Arquivo já existe na pasta de destino");
+        return;
+    }
+
+    File.Move(pathOrigem, pathDestino);
+}
+
+static void CriarArquivo()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+
+    if (!File.Exists(path))
+    {
+        using var sw = File.CreateText(path);
+        sw.WriteLine("População: 200MM");
+        sw.WriteLine("IDH: 0.91");
+        sw.WriteLine("Dados coletados em: 20/05/2022");
+    }
+}
+
+static void CriarDiretoriosGlobo()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "globo");
+    var dirGlobo = Directory.CreateDirectory(path);
+
+    var dirAmNorte = dirGlobo.CreateSubdirectory("America do Norte");
+    var dirAmCentral = dirGlobo.CreateSubdirectory("America Central");
+    var dirAmSul = dirGlobo.CreateSubdirectory("America do Sul");
+
+    dirAmNorte.CreateSubdirectory("USA");
+    dirAmNorte.CreateSubdirectory("Mexico");
+    dirAmNorte.CreateSubdirectory("Canada");
+
+    dirAmCentral.CreateSubdirectory("Panama");
+
+    dirAmSul.CreateSubdirectory("Brasil");
+    dirAmSul.CreateSubdirectory("Paraguai");
+}
